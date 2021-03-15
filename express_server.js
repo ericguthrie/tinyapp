f1aff9cf6979cf76433d588dc8de33d0edc0c0b9
Eric Guthrie <77245535+ericguthrie@users.noreply.github.com>
2021-03-15 14:34:37 -0700

    Add files via upload

---
+++ express_server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const PORT = 8080; // default port 8080;
+app.set("view engine", "ejs");
+
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars);
+});
+
+// app.get("/urls", (req, res) => {
+//   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+//   res.render("_header", templateVars);
+// });
+
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
+
+// module.exports = { urlDatabase }
