fccd4fb9c7f1d66f8879223388ba283e8b6deb45
Eric Guthrie <77245535+ericguthrie@users.noreply.github.com>
2021-03-15 15:18:03 -0700

    Add files via upload

--- express_server.js
+++ express_server.js
@@ -3,15 +3,33 @@ const app = express();
 const PORT = 8080; // default port 8080;
 app.set("view engine", "ejs");
 
+function generateRandomString() {
+
+}
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+
 
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
